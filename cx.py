c1163a3bef922f4cf504e1878ca154b09606a143
Thirupathi Bandam <thirupathi.bandam@gmail.com>
2021-01-05 14:30:26 +0530

    Update cx.py

--- cx.py
+++ cx.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings("ignore")
 import pandas as pd
-import os, time, uuid
+import os, time, uuid, sys
 from tqdm import tqdm
 from google.cloud import dialogflowcx as df
 from google.protobuf.field_mask_pb2 import FieldMask
@@ -55,15 +55,23 @@ def add_training_data(intents, train_data):
 		time.sleep(1)
 
 def detect_intent(agent, text_input):
-	DIALOGFLOW_LANGUAGE_CODE = "en"
-	SESSION_ID = uuid.uuid4()
-	session_path = f"{agent}/sessions/{SESSION_ID}"
-	session_client = df.SessionsClient()
-	text_input = df.TextInput(text=text_input)
-	query_input = df.QueryInput(text=text_input, language_code=DIALOGFLOW_LANGUAGE_CODE)
-	detectIntent = df.DetectIntentRequest(session=session_path,query_input=query_input)
-	response = session_client.detect_intent(request=detectIntent)
-	return response.query_result.intent.display_name
+	try:
+		text_input = text_input[:255] #dialog 
+		DIALOGFLOW_LANGUAGE_CODE = "en"
+		SESSION_ID = uuid.uuid4()
+		session_path = f"{agent}/sessions/{SESSION_ID}"
+		session_client = df.SessionsClient()
+		text_input = df.TextInput(text=text_input)
+		query_input = df.QueryInput(text=text_input, language_code=DIALOGFLOW_LANGUAGE_CODE)
+		detectIntent = df.DetectIntentRequest(session=session_path,query_input=query_input)
+		response = session_client.detect_intent(request=detectIntent)
+		return response.query_result.intent.display_name
+	except:
+		print("exception while input")
+		print(text_input)
+		print("Unexpected error:", sys.exc_info()[0])
+		return ""
+		
 
 def create_agent(agent_name, location_path):
 	agent = df.Agent(display_name=agent_name, default_language_code=LANGUAGE_CODE, time_zone=TIME_ZONE)
@@ -223,7 +231,7 @@ def delete_agent():
 
 if __name__ == "__main__":
 	create_and_test()
-	#test_existing(train=True)
+	#test_existing()
 	#delete_agent()
 	
 	
